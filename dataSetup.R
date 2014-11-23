fe9774d2add6946f2dd10e9c78a42a5d12a70cf6
unknown <elabuel_o@hotmail.com>
2014-11-23 15:19:42 -0600

    substring variables and adding them to dataframe

--- dataSetup.R
+++ dataSetup.R
@@ -1,21 +1,15 @@
 
+##----------------------------------------------------------------------------
+## Title: Loans data analysis project (data setup)
+## Author: Armando Enriquez Z.
+## Date: November 23rd, 2014
+## Purpose: Cleaning and preparing data for analysis
+##----------------------------------------------------------------------------
+
 ## Loading data frame
 
 load("loansData.rda")
 str(loansData)
-
-###############################################################################
-## Exploratory graphics
-library(ggplot2)
-
-ggplot(loansData, aes(x = Amount.Requested, y = Amount.Funded.By.Investors)) + 
-         geom_point()
-
-ggplot(loansData, aes(x = Amount.Requested, y = Amount.Funded.By.Investors, 
-                      colour = Home.Ownership)) + geom_point()
-
-ggplot(loansData, aes(x = Amount.Requested, y = Amount.Funded.By.Investors, 
-                      colour = Loan.Length)) + geom_point()
 
 ###############################################################################
 ## Dataset setup
@@ -24,20 +18,39 @@ names(loansData) <- c("request", "funded", "interest", "months", "purpose",
                       "creditLines", "creditBal", "inquiries", "employ")
 
 ### substring character and convert to numeric
-### Variables: interest, debtIncome and fico
+### variables: interest, debtIncome and fico
 library(gsubfn)
 
-## substring w/ regular expressions:
-interest <- loansData[, 3] ## subsetting
+### substring w/ regular expressions:
+interest <- loansData[, 3] ## subsetting (variable number 3)
 interest <- as.vector(interest)
 interest <- strapplyc(interest, "(.*)%", simplify = TRUE) ## regexp
 interest <- as.numeric(interest)
 loansData$interestNum <- interest
 
-debtIncome <- loansData[, 6] ## subsetting
+debtIncome <- loansData[, 6] ## subsetting (variable number 6)
 debtIncome <- as.vector(debtIncome)
 debtIncome <- strapplyc(debtIncome, "(.*)%", simplify = TRUE) ## regexp
 debtIncome <- as.numeric(interest)
 loansData$debtIncomeNum <- debtIncome
 
-fico <- loansData[]
+### for FICO rate, we will take two variables (low and high rate)
+fico <- loansData[, 10] ## subsetting (variable number 10)
+fico <- as.vector(fico)
+fico.low <- substr(fico, 1, 3)
+fico.high <- substr(fico, 5, 7)
+ficoLow <- as.numeric(fico.low)
+ficoHigh <- as.numeric(fico.high)
+loansData$ficoLow <- ficoLow
+loansData$ficoHigh <- ficoHigh
+
+###############################################################################
+## Exploratory graphics
+library(ggplot2)
+
+ggplot(loansData, aes(x = request, y = funded, 
+                      colour = home)) + geom_point()
+
+ggplot(loansData, aes(x = request, y = Amount.Funded.By.Investors, 
+                      colour = Loan.Length)) + geom_point()
+
