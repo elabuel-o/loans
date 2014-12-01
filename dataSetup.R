621cf6fd93c56a8ce9018dee71e45b872f1b95aa
elabuel-o <a_enriquez@live.com.mx>
2014-12-01 12:51:49 -0600

    commiting final changes

--- dataSetup.R
+++ dataSetup.R
@@ -84,13 +84,18 @@ result <- result[order(result$order), ]
 ## Removing auxiliary objects
 remove(idaho, maine, nd, nebraska, stateLoans, states, ten, state.names)
 
+## Log of the numLoans
+result$logLoans <- log(result$numLoans)
+loansData$Plazo <- loansData$months
+
 ### the map (again)
 library(tikzDevice)
 tikz(file = "USmap.tex", standAlone = TRUE)
-ggplot(result, aes(x = long, y = lat, group = group, fill = numLoans)) +
+ggplot(result, aes(x = long, y = lat, group = group, fill = logLoans)) +
         geom_polygon(colour = "black") + 
         scale_fill_gradient(low = "gray85", high = "black") +
-        coord_map("polyconic") + xlab("") + ylab("")
+        coord_map("polyconic") + xlab("") + ylab("") + 
+        labs(fill = "Número de créditos\n(logs)")
 dev.off()
 
 ##-----------------------------------------------------------------------------
@@ -98,22 +103,25 @@ dev.off()
 
 ### scatter plots
 ggplot(loansData, aes(x = fico, y = interest, 
-                      colour = months)) + 
+                      colour = Plazo)) + 
         geom_point(position = position_jitter(width = 0.7, height = 0.3)) +
-        stat_smooth(method = lm, level = 0.99, fullrange = TRUE)
+        stat_smooth(method = lm, level = 0.99, fullrange = TRUE) +
+        xlab("Calificación Crediticia") + ylab("Tasa de Interés")
 
 ggplot(loansData, aes(x = debtIncome, y = interest)) + 
         geom_point(position = position_jitter(width = 0.7, height = 0.3)) + 
-        stat_smooth(method = lm, level = 0.99, fullrange = TRUE)
+        stat_smooth(method = lm, level = 0.99, fullrange = TRUE) + 
+        xlab("Relación deuda/ingreso") + ylab("Tasa de Interés")
 
 ### histograms and density plots
-ggplot(loansData, aes(x = interest, fill = months)) + 
-  geom_histogram(binwidth = 0.73, position = "identity", alpha = 0.4)
+ggplot(loansData, aes(x = interest, fill = Plazo)) + 
+  geom_histogram(binwidth = 0.73, position = "identity", alpha = 0.4) + 
+        xlab("Tasas de Interés por plazo") + ylab("Frecuencia")
 
 ggplot(loansData, aes(x = interest, y = ..density..)) + 
         geom_histogram(binwidth = 0.73, fill = "cornsilk", 
                        colour = "gray40", size = 0.7) + 
-        geom_density(size = 0.7) + facet_grid(months ~ .)
+        geom_density(size = 0.7) + facet_grid(Plazo ~ .)
 
 ### boxplots
 
@@ -124,14 +132,19 @@ loansData$ordMedian <- with(loansData, reorder(home, funded, median))
 ggplot(loansData, aes(x = ordMedian, y = funded)) + 
         geom_boxplot() +
         geom_point(position = position_jitter(width = 0.25, height = 0.6), 
-                   alpha = 0.5) + coord_flip()
+                   alpha = 0.5) + coord_flip() + 
+        xlab("Cantidad otorgada por préstamo") + 
+        ylab("Propiedad de la vivienda")
 
+## reordering by median value (purpose)
 loansData$ordMedian2 <- with(loansData, reorder(purpose, interest, median))
 
+## the boxplot itself
 ggplot(loansData, aes(x = ordMedian2, y = interest)) + 
         geom_boxplot() + 
         geom_point(position = position_jitter(width = 0.5, height = 0.5), 
-                   alpha = 0.6) + coord_flip()
+                   alpha = 0.4) + coord_flip() +
+        xlab("Tasa de Interés") + ylab("Propósito del crédito")
 
 ggplot(loansData, aes(x = ordMedian, y = interest)) + 
         geom_boxplot() + 
